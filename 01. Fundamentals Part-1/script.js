3e67b3921e79fb7dbc863968781f00ccc8bbae43
Sachin Wanniarachchi <sachinwanniarachchi@gamil.com>
2023-08-04 12:22:20 +0530

    Added JavaScript fundamentals – part 1

---
+++ 01. Fundamentals Part-1/script.js
@@ -0,0 +1,32 @@
+/*
+////////////////////////////////////
+// Linking a JavaScript File
+let js = "amazing";
+console.log(40 + 5 + 9 - (78 * 8) / 1);
+
+////////////////////////////////////
+// Values and Variables
+console.log("Sachin");
+console.log(23);
+
+let firstName = "Sachin";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let tharindu_sachin = "TS";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+*/
