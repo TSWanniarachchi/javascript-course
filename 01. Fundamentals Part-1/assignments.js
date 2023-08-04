a422fda72ad80a7901db5eff88c05aba73a3ca04
Sachin Wanniarachchi <sachinwanniarachchi@gamil.com>
2023-08-04 12:24:08 +0530

    Completed values and variables assignment

---
+++ 01. Fundamentals Part-1/assignments.js
@@ -0,0 +1,23 @@
+/*
+////////////////////////////////////
+// Values and Variables
+*/
+// 1.
+let country = "Sri-Lanka";
+let continent = "Asia";
+let population = 22.16;
+
+// 2.
+console.log(country);
+console.log(continent);
+console.log(population);
+
+console.log(
+  "My country is " +
+    country +
+    " located in " +
+    continent +
+    ". It has a population of approximately " +
+    population +
+    " million."
+);
