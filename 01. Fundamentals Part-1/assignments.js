8d91cc408fee226eb1563e0b41ae5b3c6115f9fd
Sachin Wanniarachchi <sachinwanniarachchi@gamil.com>
2023-08-05 12:11:26 +0530

    Completed let, const and var lecture and assignment

--- 01. Fundamentals Part-1/assignments.js
+++ 01. Fundamentals Part-1/assignments.js
@@ -1,4 +1,4 @@
-/**/
+/*
 ////////////////////////////////////
 // Values and Variables
 
@@ -34,3 +34,17 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+*/
+////////////////////////////////////
+// let, const and var
+
+// 1.
+language = "Sinhala";
+
+// 2.
+const country = "Sri-Lanka";
+const continent = "Asia";
+const isIsland = true;
+
+// 3.
+// isIsland = false;
