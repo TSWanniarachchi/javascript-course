4b73e92127e45b1ec3b53a4727f241c9061efbdb
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2023-08-12 23:51:26 +0530

    Completed functions lecture

--- 02. Fundamentals Part-2/script.js
+++ 02. Fundamentals Part-2/script.js
@@ -2,7 +2,6 @@
 /*
 ////////////////////////////////////
 // Activating Strict Mode
-*/
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -11,5 +10,32 @@ if (hasDriversLicense) console.log("I can drive a 🚗");
 
 // const interface = "Audio";
 // const private = 534;
+*/
+
+///////////////////////////////////////
+// Functions
+function logger() {
+  console.log("My name is Sachin");
+}
+
+// calling, running, invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
+console.log(num);
 
 ///////////////////////////////////////
