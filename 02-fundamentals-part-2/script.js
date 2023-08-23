b49326afbf7cc95c246a1a2339ea60a98c873db8
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2023-08-23 16:49:35 +0530

    Complete while loop lecture

--- 02-fundamentals-part-2/script.js
+++ 02-fundamentals-part-2/script.js
@@ -537,6 +537,7 @@ for (let i = 0; i < sachinArray.length; i++) {
 ///////////////////////////////////////
 // Looping Backwards and Loops in Loops
 
+/*
 const sachinArray = [
   "Sachin",
   "Wanniarachchi",
@@ -559,4 +560,26 @@ for (let exercise = 1; exercise <= 3; exercise++) {
   for (let rep = 1; rep <= 5; rep++) {
     console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
   }
+}
+*/
+
+///////////////////////////////////////
+// The while Loop
+
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`FOR: Lifting weights repetition ${rep} 🏋️‍♀️`);
+// }
+
+let rep = 1;
+while (rep <= 10) {
+  // console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♀️`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log("Loop is about to end...");
 }
