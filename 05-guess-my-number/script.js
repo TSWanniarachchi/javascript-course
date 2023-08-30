426f1053332b4391680e1e9f8d57bb080eb15c01
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2023-08-30 17:47:29 +0530

    Complete selecting and manipulating elements lecture

--- 05-guess-my-number/script.js
+++ 05-guess-my-number/script.js
@@ -3,4 +3,19 @@
 ///////////////////////////////////////
 // PROJECT #1: Guess My Number!
 
+/*
 console.log(document.querySelector(".message").textContent);
+*/
+
+///////////////////////////////////////
+// Selecting and Manipulating Elements
+
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "🎉 Correct Number";
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".guess").value = 25;
+console.log(document.querySelector(".guess").value);
