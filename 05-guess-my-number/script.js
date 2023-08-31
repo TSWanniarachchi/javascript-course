e785968e0102ff01307d750776c4ae53f057a241
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2023-08-31 17:04:37 +0530

    Complete manipulating CSS styles lecture

--- 05-guess-my-number/script.js
+++ 05-guess-my-number/script.js
@@ -3,42 +3,6 @@
 ///////////////////////////////////////
 // PROJECT #1: Guess My Number!
 
-/*
-console.log(document.querySelector(".message").textContent);
-*/
-
-///////////////////////////////////////
-// Selecting and Manipulating Elements
-
-/*
-console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent = "🎉 Correct Number";
-console.log(document.querySelector(".message").textContent);
-
-document.querySelector(".secretNumber").textContent = 13;
-document.querySelector(".score").textContent = 10;
-
-document.querySelector(".guess").value = 25;
-console.log(document.querySelector(".guess").value);
-*/
-
-///////////////////////////////////////
-// Handling Click Events
-
-/*
-document.querySelector(".check").addEventListener("click", function () {
-  const guess = Number(document.querySelector(".guess").value);
-  console.log(guess, typeof guess);
-
-  if (!guess) {
-    document.querySelector(".message").textContent = "⛔ No Number!";
-  }
-});
-*/
-
-///////////////////////////////////////
-// Implementing the Game Logic
-
 // state varibles
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
@@ -50,10 +14,17 @@ document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No Number!";
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too high!";
@@ -63,6 +34,8 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "👎 You lost the game!";
       document.querySelector(".score").textContent = 0;
     }
+
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📉 Too Low!";
