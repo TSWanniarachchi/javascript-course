9f4475b0fcd07aa836db8e43e963bc38ead0b65c
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2023-08-31 12:35:41 +0530

    Complete handling click events lecture

--- 05-guess-my-number/script.js
+++ 05-guess-my-number/script.js
@@ -10,6 +10,7 @@ console.log(document.querySelector(".message").textContent);
 ///////////////////////////////////////
 // Selecting and Manipulating Elements
 
+/*
 console.log(document.querySelector(".message").textContent);
 document.querySelector(".message").textContent = "🎉 Correct Number";
 console.log(document.querySelector(".message").textContent);
@@ -19,3 +20,16 @@ document.querySelector(".score").textContent = 10;
 
 document.querySelector(".guess").value = 25;
 console.log(document.querySelector(".guess").value);
+*/
+
+///////////////////////////////////////
+// Handling Click Events
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔ No Number!";
+  }
+});
