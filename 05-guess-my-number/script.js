b143224c792b16d96df2f6a45991401c522f0fc5
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2023-09-02 21:41:48 +0530

    Complete implementing highscores lecture

--- 05-guess-my-number/script.js
+++ 05-guess-my-number/script.js
@@ -6,9 +6,11 @@
 // state varibles
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 // document.querySelector(".number").textContent = secretNumber;
 document.querySelector(".score").textContent = score;
+document.querySelector(".highscore").textContent = highscore;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -25,6 +27,12 @@ document.querySelector(".check").addEventListener("click", function () {
 
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+
+    // Set highscore
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
