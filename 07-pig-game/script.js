2fa8cc3ea7f6f19fc9d496ef93a80d8af012eaf4
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2023-09-08 01:41:13 +0530

    Complete project #3: pig game introduction lecture

--- 07-pig-game/script.js
+++ 07-pig-game/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+///////////////////////////////////////
+// PROJECT #3: Pig Game
+
+// Selecting elements
+const score0EL = document.getElementById("score--0");
+const score1EL = document.getElementById("score--1");
+const current0EL = document.querySelector("#current--0");
+const current1EL = document.querySelector("#current--1");
+const diceEL = document.querySelector(".dice");
+
+// Starting conditions
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+current0EL.textContent = 0;
+current1EL.textContent = 0;
+diceEL.classList.add("hidden");
