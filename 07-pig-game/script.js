0f4015ee93f536d32ee12d40a59e2f3cdb78de20
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2023-09-08 15:03:49 +0530

    Complete switching the active player lecture

--- 07-pig-game/script.js
+++ 07-pig-game/script.js
@@ -4,17 +4,21 @@
 // PROJECT #3: Pig Game
 
 // Selecting elements
+const player0EL = document.querySelector(".player--0");
+const player1EL = document.querySelector(".player--1");
 const score0EL = document.getElementById("score--0");
 const score1EL = document.getElementById("score--1");
-const current0EL = document.querySelector("#current--0");
-const current1EL = document.querySelector("#current--1");
+const current0EL = document.getElementById("current--0");
+const current1EL = document.getElementById("current--1");
 const diceEL = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
 // State variables
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Starting conditions
 score0EL.textContent = 0;
@@ -42,9 +46,16 @@ btnRoll.addEventListener("click", function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0EL.textContent = currentScore; // NOTE: CHANGE LATER
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0EL.classList.toggle("player--active");
+    player1EL.classList.toggle("player--active");
   }
 });
 
