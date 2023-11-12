5a49b6e9da7310b9b5cdf8d324a2d298048ca440
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2023-11-12 14:41:26 +0530

    Complete scoping in practice lecture

--- 08-behind-the-scenes/script.js
+++ 08-behind-the-scenes/script.js
@@ -1,1 +1,42 @@
 "use strict";
+
+///////////////////////////////////////
+// Scoping in Practice
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = "Tharindu";
+      // Reasssigning outer scope's variable
+      output = "NEW OUTPUT!";
+
+      var millenial = true;
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = "Sachin";
+calcAge(1996);
+
+// console.log(age);
+// printAge();
