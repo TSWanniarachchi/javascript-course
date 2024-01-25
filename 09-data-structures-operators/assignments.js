c382aac1955624dd27634ae4c442aae44dfd7017
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2024-01-25 19:09:41 +0530

    Complete destructuring arrays assignment

--- 09-data-structures-operators/assignments.js
+++ 09-data-structures-operators/assignments.js
@@ -224,3 +224,27 @@ const books = [
     highlighted: true,
   },
 ];
+
+///////////////////////////////////////
+// Destructuring Arrays
+
+// 1.
+const [firstBook, secondBook] = books;
+console.log(firstBook, secondBook);
+
+// 2.
+const [, , thirdBook] = books;
+console.log(thirdBook);
+
+// 3.
+const ratings = [
+  ["rating", 4.19],
+  ["ratingsCount", 144584],
+];
+const [[, rating], [, ratingsCount]] = ratings;
+console.log(rating, ratingsCount);
+
+// 4.
+const ratingStars = [63405, 1808];
+const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
+console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
