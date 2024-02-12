32a142bba4551d2126f445ee503eae2afc596828
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2024-02-12 19:45:54 +0530

    Complete sets lecture

--- 09-data-structures-operators/script.js
+++ 09-data-structures-operators/script.js
@@ -528,6 +528,7 @@ BONUS: Create an object called 'scorers' which contains the names of the players
 GOOD LUCK 😀
 */
 
+/*
 const game = {
   team1: "Bayern Munich",
   team2: "Borrussia Dortmund",
@@ -598,3 +599,42 @@ for (const player of game.scored) {
   scorers[player] = (scorers[player] || 0) + 1;
 }
 console.log(scorers);
+*/
+
+///////////////////////////////////////
+// Sets
+
+const ordersSet = new Set([
+  "Pasta",
+  "Pizza",
+  "Pizza",
+  "Risotto",
+  "Pasta",
+  "Pizza",
+]);
+
+console.log(ordersSet);
+console.log(ordersSet.size);
+console.log(ordersSet.has("Pizza"));
+console.log(ordersSet.has("Bread"));
+ordersSet.add("Garlic Bread");
+ordersSet.add("Garlic Bread");
+ordersSet.delete("Risotto");
+// ordersSet.clear();
+console.log(ordersSet);
+
+for (const order of ordersSet) {
+  console.log(order);
+}
+
+// Example
+const staff = ["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"];
+const staffUnique = [...new Set(staff)];
+
+console.log(staffUnique);
+console.log(
+  new Set(["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"]).size
+);
+
+console.log(new Set("Sachin"));
+console.log(new Set("Tharindu Sachin Wanniarachchi").size);
