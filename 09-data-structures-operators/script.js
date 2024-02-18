8050298d941679fb6231899049d8df563c5f672c
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2024-02-18 23:06:19 +0530

    Complete coding challenge #3

--- 09-data-structures-operators/script.js
+++ 09-data-structures-operators/script.js
@@ -645,7 +645,7 @@ console.log(new Set("Tharindu Sachin Wanniarachchi").size);
 // Maps: Fundamentals
 
 /*
-const rest = new Map();
+const rest = new Map(); 
 rest.set("name", "Classico Italiano");
 rest.set(1, "Firenze, Italy");
 console.log(rest.set(2, "Lisbon, Portugal"));
@@ -659,7 +659,7 @@ rest
 
 console.log(rest.get("name"));
 console.log(rest.get(true));
-console.log(rest.get(1));
+console.log(rest.get(1)); 
 
 const time = 8;
 console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
@@ -680,6 +680,7 @@ console.log(rest.size);
 ///////////////////////////////////////
 // Maps: Iteration
 
+/*
 const question = new Map([
   ["question", "What is the best programming language in the world?"],
   [1, "C"],
@@ -711,3 +712,57 @@ console.log([...question]);
 console.log(question.entries());
 console.log([...question.keys()]);
 console.log([...question.values()]);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/*
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3.
+const times = [...gameEvents.keys()];
+const lastEventTime = times[times.length - 1];
+console.log(
+  `An event happened, on average, every ${
+    lastEventTime / gameEvents.size
+  } minutes`
+);
+
+// 4.
+for (const [time, event] of gameEvents) {
+  console.log(
+    `[${time <= 45 ? "FIRST HALF" : "SECOND HALF"}] ${time}: ${event}`
+  );
+}
