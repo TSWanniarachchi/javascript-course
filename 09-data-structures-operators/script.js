04a2809214528f19b644edf6c7d3e5c8045fed74
TSWanniarachchi <sachinwanniarachchi@gmail.com>
2024-02-20 13:14:49 +0530

    Complete working with strings - part 3 lecture

--- 09-data-structures-operators/script.js
+++ 09-data-structures-operators/script.js
@@ -815,6 +815,7 @@ console.log(typeof new String("Sachin").slice(1));
 ///////////////////////////////////////
 // Working With Strings - Part 2
 
+/*
 const airline = "TAP Air Portugal";
 console.log(airline.toLowerCase());
 console.log(airline.toUpperCase());
@@ -871,3 +872,56 @@ const checkBaggage = function (items) {
 checkBaggage("I have a laptop, some Food and a pocket Knife");
 checkBaggage("Socks and camera");
 checkBaggage("Got some snacks and a gun for protection");
+*/
+
+///////////////////////////////////////
+// Working With Strings - Part 3
+
+// Split and join
+console.log("a+very+nice+string".split("+"));
+console.log("Sachin Wanniarchchi".split(" "));
+const [firstName, lastName] = "Sachin Wanniarchchi".split(" ");
+console.log(firstName, lastName);
+
+const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.toLowerCase().split(" ");
+  const namesUpper = [];
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(" "));
+};
+
+capitalizeName("jEssica ann smith davis");
+capitalizeName("thariNdu sachin waNniarachchi");
+
+// Padding
+const message = "Go to gate 23!";
+console.log(message.padStart(20, "+").padEnd(30, "+"));
+console.log("Sachin".padStart(20, "+").padEnd(30, "+"));
+
+const maskCreditCard = function (number) {
+  const str = String(number);
+  return str.slice(-4).padStart(str.length, "*");
+};
+
+console.log(maskCreditCard(64637836));
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard("334859493847755774747"));
+
+// Repeat
+const message2 = "Bad waether... All Departues Delayed... ";
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${"🛩".repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
